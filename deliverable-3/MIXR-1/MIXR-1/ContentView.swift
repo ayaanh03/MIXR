4f8554583b6edc0ac62e47f8d7ecbfc808c9eae3
jeffechiu <37814371+jeffechiu@users.noreply.github.com>
2020-10-29 00:16:09 -0400

    the 8th one is the one that works

---
+++ deliverable-3/MIXR-1/MIXR-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MIXR-1
+//
+//  Created by Jeffrey Chiu on 10/26/20.
+//  Copyright © 2020 443 Team 10. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ deliverable-3/MIXR-4/MIXR-4/MIXR-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  MIXR-Bridging-Header.h
+//  MIXR-4
+//
+//  Created by Jeffrey Chiu on 10/28/20.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
