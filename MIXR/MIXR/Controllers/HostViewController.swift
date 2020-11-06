4a052b4af7d41c1e09abf1efd10574a7cffc96ac
Chris Haas <cwhaas98@gmail.com>
2020-11-06 10:02:33 -0500

    Initial Commit of App w/ Storyboards

---
+++ MIXR/MIXR/Controllers/HostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HostViewController.swift
+//  MIXR
+//
+//  Created by Christopher Haas on 11/6/20.
+//
+
+
+import UIKit
+
+class HostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ MIXR/MIXR/Controllers/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  MIXR
+//
+//  Created by Christopher Haas on 11/6/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    @IBAction func signupTapped(_ sender: Any) {
+    }
+    
+}
