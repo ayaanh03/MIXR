cea28014fc41a05bed2cd586e8a693298775b321
Chris Haas <cwhaas98@gmail.com>
2020-12-03 19:04:23 -0500

    Added Music Player Storyboard, changed room creation
    Changed the Mix Room to a single storyboard that checks who the host is and displays "Generate Playlist" accordingly.

---
+++ MIXR/MIXR/Controllers/MusicPlayerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MusicPlayerViewController.swift
+//  MIXR
+//
+//  Created by Christopher Haas on 12/3/20.
+//
+
+import UIKit
+
+class MusicPlayerViewController: UIViewController {
+
+    @IBOutlet weak var songImage: UIImageView!
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
